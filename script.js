1ecaf47a3cfcf1109bbdaf6b62b8ddd1b1459d75
tran-h <43885150+tran-h@users.noreply.github.com>
2024-03-29 20:38:50 -0400

    add operate function and html/css for calculator

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let num1, num2, oper, displayText = "";
+
 function addNum(a, b) {
     return a + b;
 }
@@ -12,4 +14,23 @@ function multNum(a, b) {
 
 function divNum(a, b) {
     return a / b;
+}
+
+function operate(op, a, b) {
+    switch (op) {
+        case "+":
+            addNum(a, b);
+            break;
+        case "-":
+            subNum(a, b);
+            break;
+        case "*":
+            multNum(a, b);
+            break;
+        case "/":
+            divNum(a, b);
+            break;
+        default:
+            break;
+    }
 }
