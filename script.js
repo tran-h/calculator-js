7dd44067576e411a836466bb713ef7cfd13973a7
tran-h <43885150+tran-h@users.noreply.github.com>
2024-03-29 20:35:03 -0400

    add basic calculator operations

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNum(a, b) {
+    return a + b;
+}
+
+function subNum(a, b) {
+    return a - b;
+}
+
+function multNum(a, b) {
+    return a * b;
+}
+
+function divNum(a, b) {
+    return a / b;
+}
