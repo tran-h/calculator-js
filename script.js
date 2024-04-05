c7b4c8374121cb00a5f2dc63ac1c6fb54e1c79df
tran-h <43885150+tran-h@users.noreply.github.com>
2024-04-04 21:08:44 -0400

    fix styles, add calculator logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let num1, num2, oper, displayText = "";
+let num1 = '', num2 = '', oper = '', displayText = '', result = '';
 
 function addNum(a, b) {
     return a + b;
@@ -17,18 +17,20 @@ function divNum(a, b) {
 }
 
 function operate(op, a, b) {
+    let x = Number(a);
+    let y = Number(b);
     switch (op) {
         case "+":
-            addNum(a, b);
+            result = addNum(x, y);
             break;
         case "-":
-            subNum(a, b);
+            result = subNum(x, y);
             break;
         case "*":
-            multNum(a, b);
+            result = multNum(x, y);
             break;
         case "/":
-            divNum(a, b);
+            result = divNum(x, y);
             break;
         default:
             break;
@@ -40,7 +42,51 @@ let display = document.querySelector("#display");
 let digits = document.querySelectorAll(".digit");
 digits.forEach((digit) => {
     digit.addEventListener("click", () => {
+        if (oper != '') {
+            displayText = '';
+        }
         displayText += digit.textContent;
         display.textContent = displayText;
     });
+});
+
+let operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if (num1 == '') {
+            num1 = displayText;
+            oper = operator.textContent;
+        } else {
+            num2 = displayText;
+        }
+
+        if (num1 != '' && num2 != '') {
+            operate(oper, num1, num2);
+            oper = operator.textContent;
+            num1 = result;
+            num2 = '';
+            displayText = Math.round((result + Number.EPSILON) * 100) / 100;
+            display.textContent = displayText;
+        }
+    });
+});
+
+let btnEquals = document.querySelector("#equals");
+btnEquals.addEventListener("click", () => {
+    num2 = displayText;
+    if (num1 != '' && num2 != '') {
+        operate(oper, num1, num2);
+        displayText = Math.round((result + Number.EPSILON) * 100) / 100;
+        display.textContent = displayText;
+    }
+});
+
+let btnClear = document.querySelector("#clear");
+btnClear.addEventListener("click", () => {
+    num1 = '';
+    num2 = '';
+    oper = '';
+    displayText = '';
+    result = '';
+    display.textContent = '0';
 });
